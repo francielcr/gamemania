18e78eaf6229fa470bbc9b235e034a304bb20a38
Franciel Cantanhede <franciel_cr@hotmail.com>
2022-03-18 19:33:13 -0300

    site para hospedagem

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function mostrarmenu(){
+    let menu = document.getElementById("menu")
+
+    if (getComputedStyle(menu).display == 'none') {
+        menu.style.display = 'flex';
+    } else {
+        menu.style.display = 'none';
+    }
+}
