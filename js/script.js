073c75b3aea506286d50c824bc572074f2d88bae
Franciel Cantanhede <franciel_cr@hotmail.com>
2022-04-21 13:04:51 -0300

    gamemania

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
-function mostrarmenu(){
+function mostrarMenu() {
     let menu = document.getElementById("menu")
 
     if (getComputedStyle(menu).display == 'none') {
         menu.style.display = 'flex';
-    } else {
+    } else{
         menu.style.display = 'none';
     }
 }
+
+function cadastrarNewsletter () {
+    let email = document.getElementById("campo-email").value
+    alert(email)
+    console.log(email)
+}
+
